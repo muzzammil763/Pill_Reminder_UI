57fa7b2de4b92f7604b9bcbd810fe3a6dbd5679f
muzzammil763 <deadbase763@gmail.com>
2024-03-05 12:44:45 +0500

    added home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pill_reminder/Screens/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pill Reminder',
-      home: Scaffold(
-        body: Center(
-          child: Text('Pill Reminder'),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
